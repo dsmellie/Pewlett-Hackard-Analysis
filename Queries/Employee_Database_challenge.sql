47818410d6721125f11375332e4008cd0788ceaf
David Smellie <dsmellie@umich.edu>
2022-09-13 17:22:18 -0400

    Added Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -26,3 +26,20 @@ SELECT COUNT(ut.title), ut.title
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title);
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO mentorship_eligibilitys
+FROM employees as e
+    INNER JOIN dept_emp as de
+        ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no ASC;
